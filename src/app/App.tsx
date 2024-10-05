94f7b48f1534202df5726611738cb3f90844a985
dimajun <drankendmitrij@gmail.com>
2024-10-06 01:23:39 +0300

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <div className='font-bold'>Hello Vite</div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import App from 'app/App';
+import 'app/styles/index.css';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')!).render(<App />);
